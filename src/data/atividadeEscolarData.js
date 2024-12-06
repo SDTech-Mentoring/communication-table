c6a7006ed00563d1bcf1f9f9d43099b0bbe316ce
Ari Azevedo <azevedo.consultor@hotmail.com>
2024-12-06 17:49:34 -0300

    termino do card rotina

--- src/data/atividadeEscolarData.js
+++ src/data/atividadeEscolarData.js
@@ -6,12 +6,105 @@ const path=(name)=>{
 
 
 export const AtividadeData=[
+
+  {
+    id: 20,
+    imgSrc: path("agua"),
+    text: "ÁGUA",
+  },
+  {
+    id: 22,
+    imgSrc: path("carroEscolar"),
+    text: "TRANSPORTE ESCOLAR",
+  },
+  {
+    id: 23,
+    imgSrc: path("jogarNoLixo"),
+    text: "JOGAR LIXO",
+  },
+
+  {
+    id: 12,
+    imgSrc: path("assistirAula"),
+    text: "AULA",
+  },
+  {
+    id: 48,
+    imgSrc: path("amassar"),
+    text: "AMASSAR",
+  },
+
   {
   id:3,
   imgSrc:path('atividadesManuais'),
   text:"ATIVIDADES MANUAIS",
 
   },
+  {
+    id: 27,
+    imgSrc: path("tirarMaterial"),
+    text: "TIRAR CADERNO",
+  },
+  {
+    id: 28,
+    imgSrc: path("guardarMaterial"),
+    text: "GUARDAR CADERNOS",
+  },
+
+  {
+    id: 42,
+    imgSrc: path("recortar"),
+    text: "RECORTAR",
+  },
+  {
+    id: 43,
+    imgSrc: path("colar"),
+    text: "COLAR",
+  },
+  {
+    id: 44,
+    imgSrc: path("pintar"),
+    text: "PINTAR",
+  },
+
+  {
+    id: 45,
+    imgSrc: path("escrever"),
+    text: "ESCREVER",
+  },
+  {
+    id: 46,
+    imgSrc: path("ler"),
+    text: "LER",
+  },
+  {
+    id: 47,
+    imgSrc: path("rasgar"),
+    text: "RASGAR",
+  },
+  {
+    id: 50,
+    imgSrc: path("esporte"),
+    text: "ESPORTE",
+  },
+  {
+    id: 51,
+    imgSrc: path("reuniaoProfessor"),
+    text: "REUNIÃO COM PROFESSOR",
+  },
+
+  {
+    id: 52,
+    imgSrc: path("amigos"),
+    text: "AMIGOS",
+  },
+  {
+    id: 53,
+    imgSrc: path("ajuda"),
+    text: "AJUDA DO PROFESSOR",
+  },
+
+
 
 
 
--- src/data/schoolRoutine.js
+++ src/data/schoolRoutine.js
@@ -8,18 +8,8 @@ export const RotinaData = [
 
 
 
-  {
-    id: 12,
-    imgSrc: path("fazerAChamada"),
-    text: "FAZER A CHAMADA",
-  },
 
 
-  {
-    id: 20,
-    imgSrc: path("agua"),
-    text: "ÁGUA",
-  },
   {
     id: 21,
     imgSrc: path("materialEscolar"),
@@ -46,85 +36,13 @@ export const RotinaData = [
     rota: "/atividade-escolar",
   },
 
-  {
-    id: 22,
-    imgSrc: path("carroEscolar"),
-    text: "TRANSPORTE ESCOLAR",
-  },
-  {
-    id: 23,
-    imgSrc: path("jogarNoLixo"),
-    text: "LIXO",
-  },
 
-  {
-    id: 27,
-    imgSrc: path("tirarMaterial"),
-    text: "TIRAR CADERNO",
-  },
-  {
-    id: 28,
-    imgSrc: path("guardarMaterial"),
-    text: "GUARDAR CADERNOS",
-  },
 
-  {
-    id: 42,
-    imgSrc: path("recortar"),
-    text: "RECORTAR",
-  },
-  {
-    id: 43,
-    imgSrc: path("colar"),
-    text: "COLAR",
-  },
-  {
-    id: 44,
-    imgSrc: path("pintar"),
-    text: "PINTAR",
-  },
-  {
-    id: 45,
-    imgSrc: path("escrever"),
-    text: "ESCREVER",
-  },
-  {
-    id: 46,
-    imgSrc: path("ler"),
-    text: "LER",
-  },
-  {
-    id: 47,
-    imgSrc: path("rasgar"),
-    text: "RASGAR",
-  },
-  {
-    id: 48,
-    imgSrc: path("amassar"),
-    text: "AMASSAR",
-  },
 
-  {
-    id: 50,
-    imgSrc: path("esporte"),
-    text: "ESPORTE",
-  },
-  {
-    id: 51,
-    imgSrc: path("reuniaoProfessor"),
-    text: "REUNIÃO COM PROFESSOR",
-  },
 
-  {
-    id: 52,
-    imgSrc: path("amigos"),
-    text: "AMIGOS",
-  },
-  {
-    id: 53,
-    imgSrc: path("ajuda"),
-    text: "AJUDA DO PROFESSOR",
-  },
+
+
+
 
   // {
   //   id: 141,
